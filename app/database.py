968dc1c12ced90db80d416a58824336cad5b8d91
Joao Pedro Mazza <mazzajp27@gmail.com>
2025-10-20 15:38:43 -0300

    Adicionando bibliotecas no requirements.txt

--- app/database.py
+++ app/database.py
@@ -7,18 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Para usar SQLite (arquivo local)
-# banco = os.environ.get('BANCO')
-# DATABASE_URL = f'sqlite:///./{banco}.db'
+banco = os.environ.get('BANCO')
+DATABASE_URL = f'sqlite:///./{banco}.db'
 
 # Se depois quiser trocar para PostgreSQL, é só comentar o de cima e descomentar esse:
 # Obtém as configurações do banco de dados através das variáveis de ambiente
-usuario = os.getenv('USUARIO')
-senha = os.getenv('SENHA')
-banco = os.getenv('BANCO')
-ambiente = os.getenv('AMBIENTE')
-porta = os.getenv('PORTA')
+# usuario = os.getenv('USUARIO')
+# senha = os.getenv('SENHA')
+# banco = os.getenv('BANCO')
+# ambiente = os.getenv('AMBIENTE')
+# porta = os.getenv('PORTA')
 
-DATABASE_URL = f'postgresql://{usuario}:{senha}@{ambiente}:{porta}/{banco}'
+# DATABASE_URL = f'postgresql://{usuario}:{senha}@{ambiente}:{porta}/{banco}'
 
 # Criação do engine
 engine = create_engine(
